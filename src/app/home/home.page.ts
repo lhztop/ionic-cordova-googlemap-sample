3e8b044e3fd3ae7ff7df5f525c405eedd2ec3bf8
lhz <lhztop@163.com>
2021-07-07 12:31:24 +0800

    google map test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +17,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map: GoogleMap;
 
-  constructor() {}
+  constructor(public alertCtrl: AlertController) {}
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+      'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    });
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 39.99188446,
+           lng: 116.46706996
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 39.993214,
+        lng: 116.473222
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
+
+  async alert(message: string) {
+    let myalert= await this.alertCtrl.create({message: message});
+    await myalert.present();
+  }
 
 }
